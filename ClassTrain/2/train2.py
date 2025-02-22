b3dd7b95add209021150091f16724fda7ce76599
Your Name <you@example.com>
2025-02-23 00:16:52 +0330

    update

---
+++ ClassTrain/2/train2.py
@@ -0,0 +1,7 @@
+count=0
+for num in range(10):
+    number=int(input("Enter Number :"))
+    if number % 4 == 0 :
+        count +=1
+
+print("Count of numbers :",count)
---
+++ ClassTrain/2/train4.py
@@ -0,0 +1,25 @@
+num=int(input("Enter number (range 1,7): "))
+while True:
+    if num==0:
+        break
+    elif num == 1 :
+        print("saturady")
+        num=int(input("Enter number (range 1,7): "))
+    elif num == 2 :
+        print("sunday")
+        num=int(input("Enter number (range 1,7): "))
+    elif num == 3 :
+        print("monday")
+        num=int(input("Enter number (range 1,7): "))
+    elif num == 4 :
+        print("thirdday")
+        num=int(input("Enter number (range 1,7): "))
+    elif num == 5 :
+        print("wendsday")
+        num=int(input("Enter number (range 1,7): "))
+    elif num == 6 :
+        print("thuesday")
+        num=int(input("Enter number (range 1,7): "))
+    elif num == 7 :
+        print("friday")
+        num=int(input("Enter number (range 1,7): "))
